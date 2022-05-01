dc2c73562721842212e088a8c786498814a443d4
sahinefe <67283777+alper-efe-sahin@users.noreply.github.com>
2022-05-01 20:13:16 +0300

    style: move auth_text_field file to widgets

--- lib/presentation/widgets/sign_in_page_widgets/sign_in_page_input_fields.dart
+++ lib/presentation/widgets/sign_in_page_widgets/sign_in_page_input_fields.dart
@@ -1,5 +1,5 @@
 import 'package:bloc_login/application/login/login_cubit.dart';
-import 'package:bloc_login/domain/auth_models/auth_text_field.dart';
+import 'package:bloc_login/presentation/widgets/auth_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc_login/domain/auth_models/email.dart';
